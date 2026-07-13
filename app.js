637c01ec05aae32f1bb2c2aa8d2f62afd697ff3a
vanishreeganagi7777-bit <vanishreeganagi7777@gmail.com>
2026-07-13 15:15:22 +0530

    Updated location tracking feature

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const io=socketio(server);
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
-app.set(express.static(path.join(_dirname, "public"))); 
+
 
 io.on("connection", function(socket){
     socket.on("send-location", function(data){
@@ -27,6 +27,8 @@ app.get("/", function(req, res){
     res.render("index");
 });
 
-server.listen(3000,()=>{
-    console.log("server started");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
